d30dd07b32c7fe67bb1138db560513e14d68c287
dastan <xypxzy@gmail.com>
2023-11-20 22:38:38 +0600

    add useTheme hooks, add ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import ThemeProvider from '@/context/ThemeProvider'
 import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
@@ -26,19 +27,19 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<ClerkProvider
-			appearance={{
-				elements: {
-					formButtonPrimary: 'primary-gradient',
-					footerActionLink: 'primary-text-gradient hover:text-primary-500',
-				},
-			}}
-		>
-			<html lang='en'>
-				<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-					{children}
-				</body>
-			</html>
-		</ClerkProvider>
+		<html lang='en'>
+			<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+				<ClerkProvider
+					appearance={{
+						elements: {
+							formButtonPrimary: 'primary-gradient',
+							footerActionLink: 'primary-text-gradient hover:text-primary-500',
+						},
+					}}
+				>
+					<ThemeProvider>{children}</ThemeProvider>
+				</ClerkProvider>
+			</body>
+		</html>
 	)
 }
