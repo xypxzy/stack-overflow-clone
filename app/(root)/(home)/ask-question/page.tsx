e5552826f0177d26189ce0adf41ffe4ec08e3a84
dastan <xypxzy@gmail.com>
2023-11-26 22:15:36 +0600

    add question page, add form inputs

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -1,3 +1,12 @@
+import Question from '@/components/forms/Question/Question'
+
 export default function page() {
-	return <div>AskQuestion</div>
+	return (
+		<div>
+			<h1 className='h1-bold text-dark100_light900 '>Ask a Question</h1>
+			<div className='mt-9'>
+				<Question />
+			</div>
+		</div>
+	)
 }
