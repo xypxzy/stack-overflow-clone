60ff3cac496c4347ed44ac4b156d37931d7510c7
dastan <xypxzy@gmail.com>
2023-12-10 21:33:36 +0600

    add api/webhook to public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-	publicRoutes: ['/'],
+	publicRoutes: [
+		'/',
+		'/api/webhook',
+		'/question/:id',
+		'/tags',
+		'/tags/:id',
+		'/community',
+		'/jobs',
+	],
+	ignoredRoutes: ['/api/webhook', '/api/chatgpt'],
 })
 export const config = {
 	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
